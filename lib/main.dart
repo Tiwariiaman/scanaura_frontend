8cfd9fe5df844a09905702241fea37bf0d2ea621
TiwariiAman <tiwariaman8205@gmail.com>
2026-09-01 20:25:36 +0530

    Remove PWA and enable clean QR URLs

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,13 @@ import 'app/app.dart';
 import 'app/router/app_router.dart';
 import 'features/auth/presentation/providers/auth_notifier.dart';
 import 'features/auth/presentation/providers/auth_state.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  usePathUrlStrategy();
 
   final container = ProviderContainer();
 
